e0c466e5c3b8bd06fc30a57ffe38db52dc2d644b
Senthil Kumaran <senthilx@amazon.com>
2022-09-10 12:30:47 -0700

    Update default cluster.

--- src/data/primary-options.js
+++ src/data/primary-options.js
@@ -1,20 +1,5 @@
 const options = [
   { value: 'create', label: 'create a cluster' },
-  { value: 'associate', label: 'associate resource' },
-  { value: 'revert', label: 'create' },
-  { value: 'initialize', label: 'delete' },
-  { value: 'modify', label: 'deregister' },
-  { value: 'show', label: 'disassociate' },
-  { value: 'delete', label: 'drain' },
-  { value: 'compareCommits', label: 'enable' },
-  { value: 'configure', label: 'get' },
-  { value: 'clone', label: 'register' },
-  { value: 'ignore', label: 'scale' },
-  { value: 'rename', label: 'set' },
-  { value: 'merge', label: 'unset' },
-  { value: 'squash', label: 'upgrade' },
-  { value: 'stash', label: 'utils' },
-  { value: 'debug', label: 'version' },
 ];
 
 export const primaryOptions = options.sort((x, y) => {
