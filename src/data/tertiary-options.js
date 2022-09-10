1bc04a50bef0e71a291eb26de955a282ac3495b7
Senthil Kumaran <senthilx@amazon.com>
2022-09-10 12:45:28 -0700

    Update tertiary options.

--- src/data/tertiary-options.js
+++ src/data/tertiary-options.js
@@ -2,9 +2,14 @@ export const tertiaryOptions = {
   'default-cluster': [
     {
       value: 'default-cluster-option',
-      label: 'default cluster',
+      label: 'a default cluster',
       usage: 'eksctl create cluster',
-      nb: 'Creates a default EKS cluster with a managed nodegroup and sets KUBECONFIG to the new cluster'
-    }
+      nb: 'Creates a default EKS cluster with a managed nodes.\n\nKubernetes API endpoint access will use default of {publicAccess=true, privateAccess=false} for cluster.\n\n.CloudWatch logging will not be enabled.'
+    },
+    {
+        value: 'default-cluster-dry-run',
+        label: 'dry run',
+        usage: 'eksctl create cluster --dry-run',
+        nb: 'Prints the manifest yaml for the cluster to stdout without creating the cluster.'
   ],
 };
